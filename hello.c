aa5b527f8a8eea3b4b121dbb71aa0ec1df30b263
sethstiles <42562889+sethstiles@users.noreply.github.com>
2018-08-23 08:30:02 -0500

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Seth Stiles and Jeremy Lawler
+ * Date: 2018/8/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Seth Stiles. I am currently underclared for major but plan on going into something computer related!\n");
+
+  return 0;
+}
